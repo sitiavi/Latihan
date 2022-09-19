fab355d6c29dd867e592a7db450ba46fb750c0df
sitiavi <sitiavi88@gmail.com>
2022-09-19 19:50:57 +0700

    .

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,8 @@
+<?php
+class homepage extends CI_Controller
+{
+    public function index()
+{
+    $this->load->view('home');
+}
+}
---
+++ application/views/home.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Judul</title>
+</head>
+<body>
+    <h1> Selamat datang. . . .</h1>
+</body>
+</html>
